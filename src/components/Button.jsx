e830b29a38dd6e46f7b3feea853d12aa949bde32
uniS2 <s2oie1010@gmail.com>
2023-09-08 15:30:35 +0900

    feat: ButtonLarge 컴포넌트 생성

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-// color(primary, secondary, point), size, radiuse, icon -> 따로 처리 ...
-// secondary = Boolean(), children, ...restProps
-// type: oneOf(['submit', 'reset', 'button'])
-
-export default function Button() {
-  return <button type="button" className=""></button>;
-}
