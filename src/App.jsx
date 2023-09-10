6a74bb42ff40f1d6b8dda643f0d7e331880b51f0
uniS2 <s2oie1010@gmail.com>
2023-09-11 00:59:13 +0900

    feat: tanstack query 사용을 위한 설정 및 사용
    - 쿼리 클라이언트 객체 생성 및 QueryClientProvider 범위 지정
    - TripLocalPage 내 LocalItem 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './routes';
 import { RouterProvider } from 'react-router-dom';
+
+// 쿼리 클라이언트 객체 생성
+const queryClient = new QueryClient({
+  // 모든 쿼리에 사용되는 기본 옵션
+  defaultOptions: {
+    queries: {
+      // 기본값 정하기
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      {/* Suspense */}
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        {/* Suspense */}
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/TripLocalPage.jsx
+++ src/pages/TripLocalPage.jsx
@@ -1,21 +1,43 @@
 import ButtonLarge from '@/components/ButtonLarge';
-import Header from '@/components/Header/Header';
+import TripHeader from '@/components/Header/TripHeader';
 import LocalItem from '@/components/TripLocal/LocalItem';
 import TripTitle from '@/components/TripTitle';
+import { getPocketHostImageURL, getPocketHostURL } from '@/utils/index.js';
+import { useQuery } from '@tanstack/react-query';
 
-// 여행 지역 선택 페이지
-function TripLocalPage() {
+async function fetchLocals() {
+  const response = await fetch(`${getPocketHostURL('locals')}`);
+  return await response.json();
+}
+
+export default function TripLocalPage() {
+  const { data } = useQuery(['locals'], fetchLocals, {
+    retry: 2,
+  });
+
+  // if (isLoading)
+  /* if (isError) {
+    return {error.toString()}
+  } */
+
   return (
-    <section className="flex min-h-[50rem] flex-col items-center bg-background">
+    <section className="mx-auto flex min-h-[50rem] flex-col items-center bg-background pb-[2.3125rem]">
+      <TripHeader />
       <h1 className="sr-only">여행 지역 선택 페이지</h1>
       <TripTitle
         question={'어디로 떠나시나요?'}
         guide={'여행할 지역을 선택하세요.'}
       />
-      <LocalItem />
+      <ul className="mb-9 flex w-full max-w-7xl flex-col gap-[0.625rem]">
+        {data?.items?.map((item) => (
+          <LocalItem
+            key={item.id}
+            image={getPocketHostImageURL(item)}
+            name={item.name}
+          />
+        ))}
+      </ul>
       <ButtonLarge>선택 완료</ButtonLarge>
     </section>
   );
 }
-
-export default TripLocalPage;
