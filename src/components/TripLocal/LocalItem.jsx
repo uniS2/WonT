ccc26b8d9e4345ba4adc01f24f2c2830b7df40d8
uniS2 <s2oie1010@gmail.com>
2023-09-12 09:07:33 +0900

    feat: TripLocalPage - 기능 구현 완료
    - Zustand 라이브러리 이용한 버튼 선택-취소 기능을 구현 완료하였습니다. (단일 선택만 가능)

--- src/components/TripLocal/LocalItem.jsx
+++ src/components/TripLocal/LocalItem.jsx
@@ -1,15 +1,27 @@
 import ButtonSmall from '@/components/TripLocal/ButtonSmall';
 import LocalImage from '@/components/TripLocal/LocalImage';
 import LocalName from '@/components/TripLocal/LocalName';
+import { useLocalStore } from '@/store/localStore';
+// import { useState } from 'react';
 
-export default function LocalItem({ image, name = '지역명' }) {
+export default function LocalItem({ image, name = '지역명', index }) {
+  const { selected, setSelectIndex } = useLocalStore();
+  const isSelect = selected === index;
+
   return (
     <li className="flex items-center justify-between bg-white px-5 py-[0.9375rem]">
       <div className="flex items-center gap-5">
         <LocalImage image={image} />
         <LocalName>{name}</LocalName>
       </div>
-      <ButtonSmall>선택</ButtonSmall>
+      <ButtonSmall
+        onClick={() => {
+          setSelectIndex(index);
+        }}
+        color={isSelect ? 'bg-point' : 'bg-primary'}
+      >
+        {isSelect ? '취소' : '선택'}
+      </ButtonSmall>
     </li>
   );
 }
--- src/pages/TripLocalPage.jsx
+++ src/pages/TripLocalPage.jsx
@@ -1,19 +1,39 @@
+import { Link } from 'react-router-dom';
 import ButtonLarge from '@/components/ButtonLarge';
 import TripHeader from '@/components/Header/TripHeader';
 import LocalItem from '@/components/TripLocal/LocalItem';
 import TripTitle from '@/components/TripTitle';
 import { getPocketHostImageURL, getPocketHostURL } from '@/utils/index.js';
 import { useQuery } from '@tanstack/react-query';
+import { useLocalStore } from '@/store/localStore';
 
 async function fetchLocals() {
   const response = await fetch(`${getPocketHostURL('locals')}`);
   return await response.json();
 }
 
+/* async function fetchMySchedules() {
+  const responseData = await pocketbase.collection('mySchedules').getFullList();
+  const response = responseData.items;
+  console.log(response);
+  return await response;
+}
+
+fetchMySchedules();*/
+/* const setMySchedules = () => {
+  const { data } = useQuery(['fetchMySchedule'], fetchMySchedules, {
+    retry: 2,
+  });
+}; */
+
 export default function TripLocalPage() {
+  // Tanstack Query
   const { data } = useQuery(['locals'], fetchLocals, {
     retry: 2,
   });
+
+  const selected = useLocalStore((state) => state.selected);
+  const isSelected = selected !== null;
 
   // if (isLoading)
   /* if (isError) {
@@ -29,15 +49,22 @@ export default function TripLocalPage() {
         guide={'여행할 지역을 선택하세요.'}
       />
       <ul className="mb-9 flex w-full max-w-7xl flex-col gap-[0.625rem]">
-        {data?.items?.map((item) => (
+        {data?.items?.map((item, index) => (
           <LocalItem
             key={item.id}
             image={getPocketHostImageURL(item)}
             name={item.name}
+            index={index}
           />
         ))}
       </ul>
-      <ButtonLarge>선택 완료</ButtonLarge>
+      {isSelected ? (
+        <Link to="/tripcalendar">
+          <ButtonLarge>선택 완료</ButtonLarge>
+        </Link>
+      ) : (
+        <ButtonLarge>선택 완료</ButtonLarge>
+      )}
     </section>
   );
 }
