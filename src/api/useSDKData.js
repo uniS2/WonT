454d2862786fbbf5c58b166530f5852600606099
uniS2 <s2oie1010@gmail.com>
2023-09-13 10:01:22 +0900

    comment. 설정 변경 및 주석 정리

--- src/api/useSDKData.js
+++ src/api/useSDKData.js
@@ -25,28 +25,3 @@ export function useSDKData(collection) {
     SDK
   })
 }
-
-// const {data, status, SDK} = useSDKData(변수)
-
-/* export function useSDKData(collection) {
-  const [data, setData] = useState(null);
-  const [status, setStatus] = useState('pending');
-
-  async function SDK() {
-    try {
-      setStatus('loading');
-      // opction 변경하기 - PocketHost List/Search 참고
-      const SDKItems = await pocketbase.collection(collection).getFullList();
-      setData(SDKItems);
-      setStatus('success');
-    } catch (error) {
-      setStatus('error');
-    }
-  }
-
-  return {
-    data,
-    status,
-    SDK
-  }
-} */
