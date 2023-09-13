4bea0b0700edafd5511e1608eec310bc936a5eb8
uniS2 <s2oie1010@gmail.com>
2023-09-13 18:20:54 +0900

    feat: TripHotel/TripPlacePage - 하단 마크업 및 기본 기능 구현
    - 리스트 렌더링을 위한 컴포넌트 분리 및 작성

--- src/pages/TripHotelPage.jsx
+++ src/pages/TripHotelPage.jsx
@@ -1,24 +1,39 @@
 import TripHeader from '@/components/Header/TripHeader';
 import Map from '@/components/Map';
-import HambugerButton from '@/components/TripPlace/HambugerButton';
-import TripPlan from '@/components/TripPlan';
-import { useToggleTripPlanStore } from '@/store/toggleTripPlanStore';
+import HambugerButton from '@/components/TripSelect/HambugerButton';
+import TripPlaceItem from '@/components/TripSelect/TripPlaceItem';
+import { useToggleTripMenuStore } from '@/store/toggleTripMenuStore';
+import TripPlanMenu from '@/components/TripSelect/TripPlanMenu';
 
 export default function TripHotelPage() {
-  const { displayHotelTripPlan, toggleHotelTripPlan } =
-    useToggleTripPlanStore();
+  const {
+    displayHotelTripPlan,
+    displayHotelList,
+    toggleHotelTripPlan,
+    toggleHotelList,
+  } = useToggleTripMenuStore();
 
   return (
     <section className="container relative mx-auto">
       <h1 className="sr-only">TripHotelPage</h1>
       <TripHeader />
-      <div className="absolute z-10 w-full rounded-b-3xl bg-[#E4F5FF]">
-        {displayHotelTripPlan && <TripPlan background="bg-[#E4F5FF]" />}
-        <div className="flex justify-center">
-          <HambugerButton onClick={toggleHotelTripPlan} />
-        </div>
+      <TripPlanMenu state={displayHotelTripPlan} action={toggleHotelTripPlan} />
+      <Map width="w-full" height="min-h-[18.75rem]" className="mt-6" />
+      <ul className="mx-7 my-7 flex h-[23.1875rem] flex-col gap-[0.5625rem] overflow-y-scroll">
+        <TripPlaceItem />
+      </ul>
+      <div className="modal absolute bottom-0 z-10 w-full rounded-t-3xl bg-[#E4F5FF]">
+        <HambugerButton onClick={toggleHotelList} />
+        {displayHotelList && (
+          <div className="mx-[1.8125rem] mt-[0.5rem] min-h-[7.125rem]">
+            <h2 className="text-base font-light text-contentsPrimary ">숙소</h2>
+            <span className="absolute left-1/2 top-1/2 -translate-x-[4.5rem] text-lg font-medium text-[#5A80A9]/50">
+              숙소를 추가해주세요.
+            </span>
+            {/* <ul></ul> */}
+          </div>
+        )}
       </div>
-      <Map width="w-full" height="min-h-[18.75rem]" className="mt-[1.125rem]" />
     </section>
   );
 }
