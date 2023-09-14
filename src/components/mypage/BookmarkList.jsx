eca62d7c30a549c9c2cdbbb2d6bae3a17b32bbd8
hyeonjuuu <janghyeonjuu@gmail.com>
2023-09-14 23:58:38 +0900

    feat : BookmarkList router 수정 comment : 불필요한 주석 삭제

--- src/components/mypage/BookmarkList.jsx
+++ src/components/mypage/BookmarkList.jsx
@@ -28,13 +28,13 @@ export default function BookmarkList() {
           key={item.id}
           className="relative min-w-[360px] hover:-translate-y-1 hover:scale-x-[1.01] hover:scale-y-[1.01] hover:transition-all  hover:duration-200 hover:ease-in"
         >
+          <div
+            className="absolute right-4 top-4 cursor-pointer "
+            onClick={() => handleBookmark(item.id)}
+          >
+            <BookMark color="#C9ECFF" />
+          </div>
           <a href="/main">
-            <div
-              className="absolute right-4 top-4 cursor-pointer "
-              onClick={() => handleBookmark(item.id)}
-            >
-              <BookMark color="#C9ECFF" />
-            </div>
             <img
               src={getPocketHostImageURL(item).split(',')[0]}
               alt=""
--- src/components/mypage/Logout.jsx
+++ src/components/mypage/Logout.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Logout() {
   return (
     <a href="/signin">
