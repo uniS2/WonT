8c1cbb1a21b309c02f5f44827755e08203333d4b
uniS2 <s2oie1010@gmail.com>
2023-09-19 17:10:47 +0900

    test. 배포

--- src/routes.jsx
+++ src/routes.jsx
@@ -34,9 +34,6 @@ const router = createBrowserRouter(
       <Route path="main" element={<MainPage />} />
       <Route path="triplocal" element={<TripLocalPage />} />
       <Route path="tripcalendar" element={<TripCalendarPage />}></Route>
-      {/* <Link to={`/triplocal/${item.id}`}>
-              <button>예</button>
-            </Link> */}
       <Route path="tripedit" element={<TripEditPage />} />
       <Route path="tripplace" element={<TripPlacePage />} />
       <Route path="triphotel" element={<TripHotelPage />} />
