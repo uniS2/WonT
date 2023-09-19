32fb97c6b92812a7877ec64e4861e5d317beef51
uniS2 <134567469+uniS2@users.noreply.github.com>
2023-09-19 21:20:56 +0900

    Delete src/components/main directory

--- src/components/main/MainTitle.jsx
+++
@@ -1,9 +0,0 @@
-function MainTitle({ text = '' }) {
-  return (
-    <div className="mb-[1.625rem] h-[2.5rem] w-[20rem] border-b border-gray-200">
-      <p className="text-2xl font-bold">{text}</p>
-    </div>
-  );
-}
-
-export default MainTitle;
