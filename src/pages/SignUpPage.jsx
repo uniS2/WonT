1a8b67a5961103bec2dc1412cea92d3ae16b5a3c
sy0725 <sunyong0725@naver.com>
2023-09-20 23:09:19 +0900

    [fix] SignUpPage 접근성 개선

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -55,7 +55,12 @@ export default function SignUpPage() {
         </Link>
       </div>
       <div className="mx-auto flex min-h-[50rem] max-w-[80rem] flex-col items-center px-5">
-        <Logo width={'10.161rem'} height={'2.625rem'} isLogo={false} />
+        <Logo
+          width={'10.161rem'}
+          height={'2.625rem'}
+          isLogo={false}
+          aria-label="WonTLogoButton"
+        />
 
         <div className="flex flex-col items-center gap-[1.688rem] pt-3">
           <p>
