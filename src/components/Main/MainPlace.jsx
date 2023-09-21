e75c645256a4f97e9411c15e10308fc60aac8af2
sy0725 <sunyong0725@naver.com>
2023-09-21 23:21:12 +0900

    [fix] 테스트

--- src/components/Main/MainPlace.jsx
+++ src/components/Main/MainPlace.jsx
@@ -28,16 +28,16 @@ export default function MainPlace() {
 
   return (
     <div className="flex flex-col items-center">
-      <ul className="grid grid-cols-2 gap-5 px-[1.688rem] md:grid md:grid-cols-4 md:gap-5 lg:grid lg:grid-cols-4 lg:gap-5 2xl:grid 2xl:grid-cols-8 2xl:gap-5">
+      <ul className="grid grid-cols-2 gap-5 px-[1.688rem] md:grid md:grid-cols-4 md:gap-5 lg:grid lg:grid-cols-4 lg:gap-5 2xl:grid 2xl:grid-cols-8 2xl:gap-5 ">
         {data?.map((item) => (
           <li
             key={item.id}
-            className="relative mb-[1.625rem] flex h-[9.25rem] w-[9.25rem] transform overflow-hidden rounded-md transition hover:scale-110 hover:rounded-lg lg:h-[15rem] lg:w-[15rem] xl:h-[18.125rem] xl:w-[18.125rem] 2xl:h-[9.25rem] 2xl:w-[9.25rem]"
+            className="relative mb-[1.625rem] flex h-[9.25rem] w-[9.25rem] transform overflow-hidden rounded-md transition hover:scale-110 hover:rounded-lg lg:h-[15rem] lg:w-[15rem] xl:h-[18.125rem] xl:w-[18.125rem] 2xl:h-[9.25rem] 2xl:w-[9.25rem] "
           >
             <Link to={`/bookmark/${item.id}`}>
               <img
                 src={getPocketHostImageURL(item).split(',')[0]}
-                className="h-[9.25rem] w-[9.25rem] md:h-[9.25rem] md:w-[9.25rem] lg:h-[15rem] lg:w-[15rem] xl:h-[18.125rem] xl:w-[18.125rem]  2xl:h-[9.25rem] 2xl:w-[9.25rem]"
+                className="h-[9.25rem] w-[9.25rem] md:h-[9.25rem] md:w-[9.25rem] lg:h-[15rem] lg:w-[15rem] xl:h-[18.125rem] xl:w-[18.125rem]  2xl:h-[9.25rem] 2xl:w-[9.25rem] "
               />
               <div className="absolute bottom-1 left-1 font-bold text-white">
                 <p>{item.place}</p>
