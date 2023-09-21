3a0c0ea843a88cc3e386e95fc95de4a22051b333
sy0725 <sunyong0725@naver.com>
2023-09-21 14:49:26 +0900

    [fix] Carousel 이미지 크기 변경

--- src/components/Landing/CarouselDiv.jsx
+++ src/components/Landing/CarouselDiv.jsx
@@ -15,14 +15,19 @@ const CarouselDiv = ({ images, children }) => {
         infiniteLoop={true}
         showIndicators={false}
         transitionTime={500}
-        transitionEasing="linear"
+        transitionEasing="ease-in-out"
       >
         {images.map((image, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            // className="h-full w-full object-cover sm:h-[600px] md:h-[700px] lg:h-[850px]"
+            className="h-[26.25rem] w-[23.75rem] object-fill sm:h-[43.75rem] sm:w-[48rem] md:h-[50rem] md:w-[64rem] lg:h-[50rem] lg:w-[80rem] 2xl:h-[53.125rem] 2xl:w-[96rem]"
+          >
             <img
               src={image}
               alt={`Image ${index}`}
-              style={{ width: '1536px', height: '850px' }}
+              className="h-full w-full object-cover"
+              // style={{ width: '1536px', height: '850px' }}
             />
           </div>
         ))}
