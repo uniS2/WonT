291347c53c1bf11f9aa5d02ceea24762a412a9a4
uniS2 <s2oie1010@gmail.com>
2023-09-23 00:51:04 +0900

    fix: fetch -> pocketbase

--- src/pages/TripLocalPage.jsx
+++ src/pages/TripLocalPage.jsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+
+import pocketbase from '@/api/pocketbase';
 import ButtonLarge from '@/components/ButtonLarge';
 import TripHeader from '@/components/Header/TripHeader';
 import LocalItem from '@/components/TripLocal/LocalItem';
 import TripTitle from '@/components/TripTitle';
 import { useLocalStore } from '@/store/localStore';
-import { getPocketHostImageURL, getPocketHostURL } from '@/utils/index.js';
+import { getPocketHostImageURL } from '@/utils/index.js';
 
 async function fetchLocals() {
-  const response = await fetch(`${getPocketHostURL('locals')}`);
-  return await response.json();
+  const response = await pocketbase.collection('locals').getFullList();
+  return response;
 }
 
 export default function TripLocalPage() {
@@ -40,7 +42,7 @@ export default function TripLocalPage() {
         guide={'여행할 지역을 선택하세요.'}
       />
       <ul className="mb-9 flex w-full flex-col gap-[0.625rem] md:grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-        {data?.items?.map((item) => (
+        {data?.map((item) => (
           <LocalItem
             key={item.id}
             image={item.image ? getPocketHostImageURL(item) : null}
