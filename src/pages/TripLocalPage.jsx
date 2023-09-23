4b5b876694eb989ccd0f0753c6089a5e60cc511c
uniS2 <s2oie1010@gmail.com>
2023-09-23 16:19:04 +0900

    fix: 병합 오류 해결 중

--- src/pages/TripLocalPage.jsx
+++ src/pages/TripLocalPage.jsx
@@ -1,18 +1,15 @@
 import { Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
 import { useQuery } from '@tanstack/react-query';
-
-import pocketbase from '@/api/pocketbase';
 import ButtonLarge from '@/components/ButtonLarge';
 import TripHeader from '@/components/Header/TripHeader';
 import LocalItem from '@/components/TripLocal/LocalItem';
 import TripTitle from '@/components/TripTitle';
 import { useLocalStore } from '@/store/localStore';
-import { getPocketHostImageURL } from '@/utils/index.js';
+import { getPocketHostImageURL, getPocketHostURL } from '@/utils/index.js';
 
 async function fetchLocals() {
-  const response = await pocketbase.collection('locals').getFullList();
-  return response;
+  const response = await fetch(`${getPocketHostURL('locals')}`);
+  return await response.json();
 }
 
 export default function TripLocalPage() {
@@ -33,35 +30,30 @@ export default function TripLocalPage() {
   }
 
   return (
-    <>
-      <Helmet>
-        <title>TripLocal - WonT</title>
-      </Helmet>
-      <section className="container mx-auto flex min-h-[50rem] flex-col items-center  bg-background pb-[2.3125rem]">
-        <TripHeader isBack={false} />
-        <h1 className="sr-only">여행 지역 선택 페이지</h1>
-        <TripTitle
-          question={'어디로 떠나시나요?'}
-          guide={'여행할 지역을 선택하세요.'}
-        />
-        <ul className="mb-9 flex w-full flex-col gap-[0.625rem] md:grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-          {data?.map((item) => (
-            <LocalItem
-              key={item.id}
-              image={item.image ? getPocketHostImageURL(item) : null}
-              name={item.name}
-              index={item.id}
-            />
-          ))}
-        </ul>
-        {isSelect ? (
-          <Link to="/tripcalendar">
-            <ButtonLarge>선택 완료</ButtonLarge>
-          </Link>
-        ) : (
+    <section className="container mx-auto flex min-h-[50rem] flex-col items-center  bg-background pb-[2.3125rem]">
+      <TripHeader isBack={false} />
+      <h1 className="sr-only">여행 지역 선택 페이지</h1>
+      <TripTitle
+        question={'어디로 떠나시나요?'}
+        guide={'여행할 지역을 선택하세요.'}
+      />
+      <ul className="mb-9 flex w-full flex-col gap-[0.625rem] md:grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+        {data?.items?.map((item) => (
+          <LocalItem
+            key={item.id}
+            image={item.image ? getPocketHostImageURL(item) : null}
+            name={item.name}
+            index={item.id}
+          />
+        ))}
+      </ul>
+      {isSelect ? (
+        <Link to="/tripcalendar">
           <ButtonLarge>선택 완료</ButtonLarge>
-        )}
-      </section>
-    </>
+        </Link>
+      ) : (
+        <ButtonLarge>선택 완료</ButtonLarge>
+      )}
+    </section>
   );
 }
