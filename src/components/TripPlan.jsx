0797d34250da45e15ecb59c64ac4162f9e3ba0d2
hyeonjuuu <janghyeonjuu@gmail.com>
2023-09-24 19:37:43 +0900

    fix : 데이터 이슈 수정

--- src/components/TripPlan.jsx
+++ src/components/TripPlan.jsx
@@ -5,14 +5,13 @@ import { useDateStore } from '@/store/dateStore';
 
 export default function TripPlan({ data, background = 'bg-secondary/50' }) {
   const selectDate = useDateStore((set) => set.tripDate);
-  console.log(selectDate);
   return (
     <div className={`${background} px-6 py-[1.125rem] md:px-10 lg:px-8`}>
       <div className="mx-auto max-w-7xl">
         <div className="flex gap-2">
           <dl>
             <dt className="sr-only">장소</dt>
-            <dd className="text-base text-contentsPrimary">{data.title}</dd>
+            <dd className="text-base text-contentsPrimary">{data?.title}</dd>
           </dl>
           <Link to="/triplocal">
             <TripPlanEditButton />
