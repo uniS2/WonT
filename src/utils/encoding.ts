4778a96ccd48c2347eebb564de3958c54685653c
uniS2 <s2oie1010@gmail.com>
2024-01-05 11:17:48 +0900

    refactor: `export default` 문 하단 작성으로 변경

--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -1,4 +1,6 @@
 // Tour API > 키워드 검색 조회 > 키워드 encoding 처리
-export default function encoding(keyword: string) {
+function encoding(keyword: string) {
   return encodeURIComponent(keyword);
 }
+
+export default encoding;
