a3e39e60b3cb1a0217385c56896ffe3d429fa861
uniS2 <s2oie1010@gmail.com>
2024-01-05 10:48:17 +0900

    refactor: createRecord 유틸함수 JS -> TS 변경 - RecordData interface 지정 - error 처리를 위해 타입 가드(instanceof) 코드 작성

--- src/utils/createRecord.js
+++
@@ -1,9 +0,0 @@
-import pocketbase from "@/api/pocketbase";
-
-export const createRecord = async (collectionName, data) => {
-  try {
-    return await pocketbase.collection(collectionName).create(data);
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
