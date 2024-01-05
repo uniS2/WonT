5bcbf810a3dc47d6bb2843d599aef8f76a4f5dfd
uniS2 <s2oie1010@gmail.com>
2024-01-05 10:58:41 +0900

    refactor: debounce 유틸함수 JS -> TS 변경

--- src/utils/debounce.js
+++
@@ -1,7 +0,0 @@
-export default function debounce(callback, timeout = 300) {
-  let cleanup;
-  return (...args) => {
-    clearTimeout(cleanup);
-    cleanup = setTimeout(callback.bind(null, ...args), timeout);
-  };
-}
