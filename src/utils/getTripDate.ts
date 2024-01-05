6dbf3b858275579e8481be6e42befb04857290c2
uniS2 <s2oie1010@gmail.com>
2024-01-05 13:08:01 +0900

    refactor: getTripDate 유틸함수 JS -> TS 변경 - getTripDateKo 사용되지 않고 있으나 필요하다 판단하여 export 선언

--- src/utils/getTripDate.ts
+++ src/utils/getTripDate.ts
@@ -8,31 +8,39 @@ const dayText = [
   '토요일',
 ];
 
-export function getTripDate(date) {
+function getTripDate(date: string) {
   return date.slice(0, 10);
 }
 
-export function getTripDateKo(dates) {
+function getTripDateKo(dates: Date) {
   const month = dates.getMonth() + 1;
   const date = dates.getDate();
   const day = dayText[dates.getDay()];
   return `${month}월 ${date}일 ${day}`;
 }
 
-export function getTripDateUTC(dates) {
+function getTripDateUTC(dates: Date) {
   let dateUTC = new Date(
     `${String(dates).slice(0, 15)} 12:00:00`
   ).toISOString();
   return `${dateUTC.slice(0, 10)} 12:00:00.123Z`;
 }
 
-export function getTripDateDot(dates) {
+function getTripDateDot(dates: Date) {
   const year = dates.getFullYear();
   const month = dates.getMonth() + 1;
   const date = dates.getDate();
   return `${year}.${month}.${date}`;
 }
 
-export const getTripDates = (startDay, endDay) => {
+const getTripDates = (startDay: string, endDay: string) => {
   return `${startDay} - ${endDay}`;
+};
+
+export {
+  getTripDate,
+  getTripDateKo,
+  getTripDateUTC,
+  getTripDateDot,
+  getTripDates,
 };
