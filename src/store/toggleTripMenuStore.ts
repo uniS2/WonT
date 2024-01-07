9af9d18723aba68e2370f0b2d31087d9b64b4b00
hyeonjuuu <janghyeonjuu@gmail.com>
2024-01-07 23:36:22 +0900

    refactor : ToggleTotalSchedule 타입 정의

---
+++ src/store/toggleTripMenuStore.ts
@@ -0,0 +1,79 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+export const useToggleTripMenuStore = create(
+  devtools((set) => ({
+    // state
+    // 여행 장소선택 페이지
+    displayPlaceTripPlan: true,
+    displayPlaceList: true,
+    // 여행 숙소선택 페이지
+    displayHotelTripPlan: true,
+    displayHotelList: true,
+    // 나의 일정상세 페이지
+    displayTotalschedule: true,
+    displayDeleteModal: false,
+    displayDaySchedule: true,
+
+    // action
+    // 여행 장소선택 페이지
+    togglePlaceTripPlan: () =>
+      set(
+        (state: { displayPlaceTripPlan: boolean }) => ({
+          displayPlaceTripPlan: !state.displayPlaceTripPlan,
+        }),
+        false,
+        'placeTripPlan/toggler'
+      ),
+    togglePlaceList: () =>
+      set(
+        (state: { displayPlaceList: boolean }) => ({
+          displayPlaceList: !state.displayPlaceList,
+        }),
+        false,
+        'placeList/toggler'
+      ),
+    // 여행 숙소선택 페이지
+    toggleHotelTripPlan: () =>
+      set(
+        (state: { displayHotelTripPlan: boolean }) => ({
+          displayHotelTripPlan: !state.displayHotelTripPlan,
+        }),
+        false,
+        'hotelTripPlan/toggler'
+      ),
+    toggleHotelList: () =>
+      set(
+        (state: { displayHotelList: boolean }) => ({
+          displayHotelList: !state.displayHotelList,
+        }),
+        false,
+        'hotelList/toggler'
+      ),
+    // 나의 일정상세 페이지
+    toggleTotalschedule: () =>
+      set(
+        (state: { displayTotalschedule: boolean }) => ({
+          displayTotalschedule: !state.displayTotalschedule,
+        }),
+        false,
+        'totalSchedule/toggler'
+      ),
+    toggleDeleteModal: () =>
+      set(
+        (state: { displayDeleteModal: boolean }) => ({
+          displayDeleteModal: !state.displayDeleteModal,
+        }),
+        false,
+        'deleteModal/toggler'
+      ),
+    toggleDaySchedule: () =>
+      set(
+        (state: { displayDaySchedule: boolean }) => ({
+          displayDaySchedule: !state.displayDaySchedule,
+        }),
+        false,
+        'daySchedule/toggler'
+      ),
+  }))
+);
