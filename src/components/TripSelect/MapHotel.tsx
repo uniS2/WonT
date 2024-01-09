1b4dfb8e5ab3aa2e41c11aa12b41a182aa3f1a1a
uniS2 <s2oie1010@gmail.com>
2024-01-09 20:20:22 +0900

    refactor: Map Api 로직 JS -> TS 수정 (any 타입 추후 수정 예정) - 주석으로 any 타입 부분 표시 - kakao.map 출력 내용 확인 후 수정 예정

--- src/components/TripSelect/MapHotel.tsx
+++ src/components/TripSelect/MapHotel.tsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from 'react';
-
-import debounce from '@/utils/debounce';
 import { MapStore } from '@/store/mapStore';
+import { debounce } from '@/utils/debounce';
+
+interface MapHotelProps {
+  localName?: string;
+  width?: string;
+  height?: string;
+  latitude?: number;
+  longitude?: number;
+  level?: number;
+  restProps?: string;
+}
 
 const { kakao } = window;
 
-export default function MapPlace({
-  placeName,
+function MapHotel({
+  localName,
   width = 'w-full',
   height = 'min-h-[18.75rem] sm:h-[22rem] md:h-[26rem] lg:h-[30rem]',
   latitude = 37.4812845080678, // 위도
   longitude = 126.952713197762, // 경도
   level = 6, // 지도 확대 레벨
   restProps,
-}) {
+}: MapHotelProps) {
   const [center] = useState(new kakao.maps.LatLng(latitude, longitude));
 
-  const category = 'AT4'; // 장소 - 관광명소 카테고리
+  const hotelCategory = 'AD5'; // 숙소 카테고리
 
-  let markers = []; // 마커
-  const { setPlaceList } = MapStore(); // 장소 목록
+  let markers: any = []; //$ 마커, Type 수정 예정
+  const { setHotelList } = MapStore(); // 숙소 목록
 
   useEffect(() => {
-    const container = document.getElementById('mapPlace');
+    const container = document.getElementById('mapHotel');
     const options = {
       center: center, // 지도의 중심좌표. 33.450701, 126.570667
       level: level, // 지도의 확대 레벨. 3
@@ -56,29 +65,33 @@ export default function MapPlace({
     }
 
     //# 카테고리 검색 완료 시 호출되는 콜백함수
-    function categorySearchCB(data, status, pagination) {
+    function categorySearchCB(data: any, status: any) {
+      //$ any Type 수정 예정
+      //* pagination
       removeMarker();
-      setPlaceList(data);
+      setHotelList(data);
 
       if (status === kakao.maps.services.Status.OK) {
         for (let i = 0; i < data.length; i++) {
           displayMarker(data[i]);
         }
       } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-        alert('해당 지역에는 원하는 관광명소가 없습니다.');
+        alert('해당 지역에는 원하는 숙소가 없습니다.');
         return;
       } else if (status === kakao.maps.services.Status.ERROR) {
-        alert('검색 중 오류가 발생했습니다.');
+        alert('숙소 검색 중 오류가 발생했습니다.');
         return;
       }
     }
 
     //# 지도에 마커를 표시하는 함수
-    function displayMarker(place) {
+    function displayMarker(place: any) {
+      //$ any Type 수정 예정
       // 마커를 생성하고 지도에 표시합니다
       const marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x),
+        // image
       });
 
       markers.push(marker);
@@ -86,12 +99,12 @@ export default function MapPlace({
       const info = `<div style="margin:0.3125rem;font-size:0.75rem;background:#C9ECFF;color:#152644;">
       <dl>
       <div style="display:flex;gap:0.25rem;"><dt>숙소명</dt><dd>${place.place_name}</dd></div>
-      <div style="display:flex;gap:0.25rem;"><dt>주소</dt><dd}>${place.address_name}</dd></div>
+      <div style="display:flex;gap:0.25rem;"><dt>주소</dt><dd}>${place.road_address_name}</dd></div>
         </dl>
         <div style="display:flex;gap:0.25rem;"><span>URL: </span><a target="_blank" rel="noopener noreferrer" href=${place.place_url} style="color:#F97660">${place.place_url}</a></div></div>`;
 
       // 마커에 이벤트를 등록합니다.
-      //- click
+      //- mouseover
       kakao.maps.event.addListener(marker, 'mouseover', function () {
         infowindow.setContent(info);
         infowindow.open(map, marker);
@@ -114,19 +127,20 @@ export default function MapPlace({
     }
 
     //# 1. 현재 선택한 지역으로 좌표를 검색합니다
-    geocoder.addressSearch(placeName, function (result, status) {
+    geocoder.addressSearch(localName, function (result: any, status: any) {
+      //$ any Type 수정 예정
       if (status === kakao.maps.services.Status.OK) {
         const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
         // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
         map.setCenter(coords);
 
-        // 카테고리 - 장소 검색
+        // 카테고리 - 숙박(숙소) 검색
         const categoryOptions = {
           location: coords,
           radius: 10000,
         };
-        places.categorySearch(category, categorySearchCB, categoryOptions);
+        places.categorySearch(hotelCategory, categorySearchCB, categoryOptions);
       }
     });
 
@@ -136,18 +150,20 @@ export default function MapPlace({
       'idle',
       debounce(function () {
         const newCenter = map.getCenter();
-        places.categorySearch(category, categorySearchCB, {
+        places.categorySearch(hotelCategory, categorySearchCB, {
           location: newCenter,
           radius: 10000,
         });
       })
     );
-  }, [center, level, category, setPlaceList]);
+  }, [center, level, setHotelList]);
 
   return (
     <div
-      id="mapPlace"
+      id="mapHotel"
       className={`${height} ${width} modal mx-auto mt-6 ${restProps}`}
     ></div>
   );
 }
+
+export default MapHotel;
--- src/components/TripSelect/MapPlace.tsx
+++ src/components/TripSelect/MapPlace.tsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from 'react';
+import { debounce } from '@/utils/debounce';
+import { MapStore } from '@/store/mapStore';
 
-import { useMapStore } from '@/store/mapStore';
-import debounce from '@/utils/debounce';
+interface MapPlaceProps {
+  placeName: string;
+  width?: string;
+  height?: string;
+  latitude?: number;
+  longitude?: number;
+  level?: number;
+  restProps?: string;
+}
 
 const { kakao } = window;
 
-export default function MapHotel({
-  localName,
+function MapPlace({
+  placeName,
   width = 'w-full',
   height = 'min-h-[18.75rem] sm:h-[22rem] md:h-[26rem] lg:h-[30rem]',
   latitude = 37.4812845080678, // 위도
   longitude = 126.952713197762, // 경도
   level = 6, // 지도 확대 레벨
   restProps,
-}) {
+}: MapPlaceProps) {
   const [center] = useState(new kakao.maps.LatLng(latitude, longitude));
 
-  const hotelCategory = 'AD5'; // 숙소 카테고리
+  const category = 'AT4'; // 장소 - 관광명소 카테고리
 
-  let markers = []; // 마커
-  const { setHotelList } = useMapStore(); // 숙소 목록
+  let markers: any = []; //$ 마커, Type 수정 예정
+  const { setPlaceList } = MapStore(); // 장소 목록
 
   useEffect(() => {
-    const container = document.getElementById('mapHotel');
+    const container = document.getElementById('mapPlace');
     const options = {
       center: center, // 지도의 중심좌표. 33.450701, 126.570667
       level: level, // 지도의 확대 레벨. 3
@@ -56,30 +65,32 @@ export default function MapHotel({
     }
 
     //# 카테고리 검색 완료 시 호출되는 콜백함수
-    function categorySearchCB(data, status, pagination) {
+    function categorySearchCB(data: any, status: any) {
+      //$ any Type 수정 예정
+      //* pagination
       removeMarker();
-      setHotelList(data);
+      setPlaceList(data);
 
       if (status === kakao.maps.services.Status.OK) {
         for (let i = 0; i < data.length; i++) {
           displayMarker(data[i]);
         }
       } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-        alert('해당 지역에는 원하는 숙소가 없습니다.');
+        alert('해당 지역에는 원하는 관광명소가 없습니다.');
         return;
       } else if (status === kakao.maps.services.Status.ERROR) {
-        alert('숙소 검색 중 오류가 발생했습니다.');
+        alert('검색 중 오류가 발생했습니다.');
         return;
       }
     }
 
     //# 지도에 마커를 표시하는 함수
-    function displayMarker(place) {
+    function displayMarker(place: any) {
+      //$ any Type 수정 예정
       // 마커를 생성하고 지도에 표시합니다
       const marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x),
-        // image
       });
 
       markers.push(marker);
@@ -87,12 +98,12 @@ export default function MapHotel({
       const info = `<div style="margin:0.3125rem;font-size:0.75rem;background:#C9ECFF;color:#152644;">
       <dl>
       <div style="display:flex;gap:0.25rem;"><dt>숙소명</dt><dd>${place.place_name}</dd></div>
-      <div style="display:flex;gap:0.25rem;"><dt>주소</dt><dd}>${place.road_address_name}</dd></div>
+      <div style="display:flex;gap:0.25rem;"><dt>주소</dt><dd}>${place.address_name}</dd></div>
         </dl>
         <div style="display:flex;gap:0.25rem;"><span>URL: </span><a target="_blank" rel="noopener noreferrer" href=${place.place_url} style="color:#F97660">${place.place_url}</a></div></div>`;
 
       // 마커에 이벤트를 등록합니다.
-      //- mouseover
+      //- click
       kakao.maps.event.addListener(marker, 'mouseover', function () {
         infowindow.setContent(info);
         infowindow.open(map, marker);
@@ -115,19 +126,20 @@ export default function MapHotel({
     }
 
     //# 1. 현재 선택한 지역으로 좌표를 검색합니다
-    geocoder.addressSearch(localName, function (result, status) {
+    geocoder.addressSearch(placeName, function (result: any, status: any) {
+      //$ any Type 수정 예정
       if (status === kakao.maps.services.Status.OK) {
         const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
         // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
         map.setCenter(coords);
 
-        // 카테고리 - 숙박(숙소) 검색
+        // 카테고리 - 장소 검색
         const categoryOptions = {
           location: coords,
           radius: 10000,
         };
-        places.categorySearch(hotelCategory, categorySearchCB, categoryOptions);
+        places.categorySearch(category, categorySearchCB, categoryOptions);
       }
     });
 
@@ -137,18 +149,20 @@ export default function MapHotel({
       'idle',
       debounce(function () {
         const newCenter = map.getCenter();
-        places.categorySearch(hotelCategory, categorySearchCB, {
+        places.categorySearch(category, categorySearchCB, {
           location: newCenter,
           radius: 10000,
         });
       })
     );
-  }, [center, level, setHotelList]);
+  }, [center, level, category, setPlaceList]);
 
   return (
     <div
-      id="mapHotel"
+      id="mapPlace"
       className={`${height} ${width} modal mx-auto mt-6 ${restProps}`}
     ></div>
   );
 }
+
+export default MapPlace;
