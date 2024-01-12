0687c5b80a55fc60a4bdcec2dcddd94547d73d8c
uniS2 <s2oie1010@gmail.com>
2024-01-12 14:13:35 +0900

    bug: 헤더 일정 부분 오류로 인한 `TripPlan`주석 처리

--- src/components/TripSelect/TripPlanMenu.tsx
+++ src/components/TripSelect/TripPlanMenu.tsx
@@ -1,6 +1,5 @@
-import TripPlan from '@/components/TripPlan';
+//$ import TripPlan from '@/components/TripPlan';
 import HambugerButton from '@/components/TripSelect/HambugerButton';
-//$ import MySchedule from '@/pages/MyPage/MySchedule';
 
 interface TripPlanMenuProps {
   state: boolean;
@@ -11,7 +10,7 @@ interface TripPlanMenuProps {
 function TripPlanMenu({ state, action, data }: TripPlanMenuProps) {
   return (
     <div className="modal absolute z-20 w-full rounded-b-3xl bg-[#E4F5FF]">
-      {state && <TripPlan data={data} background="bg-[#E4F5FF]" />}
+      {/* {state && <TripPlan data={data} background="bg-[#E4F5FF]" />} */}
       <HambugerButton onClick={action} />
     </div>
   );
